7e2fce4a7a95f7011cebadf9e0d2cd550548176f
Ihar Mezentsau <iharmezentsau@gmail.com>
2019-01-08 23:56:36 +0300

    RainbowFrame

---
+++ RainbowFrame/App.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame';
+
+let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'],
+    text = 'Hello';
+
+ReactDOM.render(<RainbowFrame colors={colors}>{text}</RainbowFrame>, document.getElementById('container'));
+
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './RainbowFrame.css';
+
+const RainbowFrame = (props) => {
+    const arrayColorsLength = props.colors.length,
+        i = 0;
+
+    if (i < arrayColorsLength) {
+        return (
+            <div style={{border: `solid 5px ${props.colors[i]}`, padding: "10px"}}>
+                <RainbowFrame colors={props.colors.slice(1)}>
+                    {props.children}
+                </RainbowFrame>
+            </div>
+        );
+    } else
+        return props.children;
+
+};
+
+RainbowFrame.propTypes = {
+    colors: PropTypes.array.isRequired,
+    //text: PropTypes.string.isRequired,
+};
+
+export default RainbowFrame;
