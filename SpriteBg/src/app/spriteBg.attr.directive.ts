abef924dd144a4f6de4fcec720fa31bf76118591
Ihar Mezentsau <iharmezentsau@gmail.com>
2019-03-01 16:48:25 +0300

    SpriteBg

--- SpriteBg/src/app/spriteBg.attr.directive.ts
+++ SpriteBg/src/app/spriteBg.attr.directive.ts
@@ -5,21 +5,6 @@ import { Directive, ElementRef, Input, Attribute } from "@angular/core";
 })
 export class SpriteBgDirective {
 
-  //@Input("url")
-  //public url: string;
-  //
-  // @Input("offset-x")
-  // public offsetX: number;
-  //
-  // @Input("offset-y")
-  // public offsetY: number;
-  //
-  // @Input("width")
-  // public width: number;
-  //
-  // @Input("height")
-  // public height: number;
-
   constructor(private element: ElementRef, @Attribute("spriteBg-url") url: string,
     @Attribute("spriteBg-offset-x") offsetX: number, @Attribute("spriteBg-offset-y") offsetY: number,
     @Attribute("spriteBg-width") width: number, @Attribute("spriteBg-height") height: number) {
@@ -34,29 +19,5 @@ export class SpriteBgDirective {
     this.element.nativeElement.style.width = `${width}px`;
     this.element.nativeElement.style.height = `${height}px`;
   }
-
-
-  // colors:Array<string>=['red','green','blue','cyan','magenta','yellow'];
-  //
-  // constructor(private element: ElementRef) {
-  //
-  //   this.setRandomColor();
-  //
-  //   setInterval( ()=>{
-  //     this.setRandomColor()
-  //   },2000);
-  //
-  // }
-  //
-  // setRandomColor():void {
-  //   let randomColorIndex:number
-  //     =Math.floor(Math.random()*this.colors.length);
-  //   let randomColor:string
-  //     =this.colors[randomColorIndex];
-  //   // element:ElementRef - это Ангуляр-обёртка для HTML-тега
-  //   // element.nativeElement - это обычный DOM-элемент
-  //   this.element.nativeElement
-  //     .style.backgroundColor=randomColor;
-  // }
 
 }
